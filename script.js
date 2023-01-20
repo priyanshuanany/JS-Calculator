c4d254a202344f42da9735c2708468b62450ac95
PRIYANSHU ANANY <ananypriyanshu@gmail.com>
2023-01-20 12:47:30 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let str = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        if(e.target.innerHTML == "="){
+            str = eval(str);
+            document.querySelector("input").value = str;
+        }
+        else if(e.target.innerHTML == "C"){
+            str = "";
+            document.querySelector("input").value = str;
+        }
+        else if(e.target.innerHTML == "%"){
+            str = eval(str)/100
+            document.querySelector("input").value = str;
+        }
+        else{
+            console.log(e.target.innerHTML)
+            str = str + e.target.innerHTML;
+            document.querySelector("input").value = str;
+        }
+    
+    })
+})
